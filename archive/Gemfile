514e54c67818a98d01f61605b5e95ff385dd8872
Joshua Michael Hall <sejoshuahall@gmail.com>
2025-01-18 12:51:42 -0600

    restarting project

--- archive/Gemfile
+++ archive/Gemfile
@@ -1,8 +1,9 @@
 source "https://rubygems.org"
 
+ruby "3.2.2"
+gem "rackup", "~> 2.2"
+gem "puma", "~> 6.5"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "erubis"
 gem "webrick"
-
-ruby "3.2.2"
--- book_viewer.rb
+++ book_viewer.rb
@@ -1,18 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
-# require "tilt/erubis"
 
 get "/" do
-  @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
-
-  erb :home
-end
-
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
-
-  erb :chapter
+  File.read "public/template.html"
 end
